42d9d55ec8ec38171e5adada318294319665672c
Marcos André de Almeida <marcos@xqx.com.br>
2018-02-17 17:10:31 -0200

    Add files via upload

---
+++ visual_c_2015/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'visual_c_2015'
+$url = 'http://xqx.com.br/public/vc_redist.x64.exe'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  silentArgs    = "/S"
+  url           = $url
+}
+
+Install-ChocolateyPackage @packageArgs
